6552528abfe2a7e06d369c31927c678ffb25a817
simranlalchandani <neeta.lalchandani@gmail.com>
2020-12-17 17:31:08 +0530

    modified

--- Chain.js
+++ Chain.js
@@ -1,20 +1,27 @@
-class Chain{
-    constructor(bodyA, bodyB){
+class SlingShot{
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
-            stiffness: 0.04,
+            pointB: pointB,
+            stiffness: 0.02,
             length: 10
         }
-        this.chain = Constraint.create(options);
-        World.add(world, this.chain);
+        this.pointB=pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    fly(){
+        this.sling.bodyA=null
     }
 
     display(){
-        var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        if(this.sling.bodyA){
+
+        
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
     }
-    
+}
 }
